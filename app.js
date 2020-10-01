a48973da5cc2f945a253a2d023f6d55427721e2d
Derrick Bills <dbills777@gmail.com>
2020-10-01 10:37:20 -0600

    create test fail deploy

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('publicc'))
 app.listen(process.env.PORT || port, ()=> console.log(`"Static Files App LIstening at http://localhost:${port}`))
