840c11c7a354c18248e4815446bf873eaa2e9fb5
Derrick Bills <dbills777@gmail.com>
2020-10-01 10:38:41 -0600

    verified it fails if server does not work

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('publicc'))
-app.listen(process.env.PORT || port, ()=> console.log(`"Static Files App LIstening at http://localhost:${port}`))
+app.use(express.static('public'));
+app.listen(process.env.PORT || port, () =>
+  console.log(`"Static Files App LIstening at http://localhost:${port}`)
+);
